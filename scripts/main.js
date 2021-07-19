ffc04218ab3887117e79127a709c8786f2fab596
José Almir <50648379+jrcodev@users.noreply.github.com>
2021-07-19 16:49:28 -0300

    First post

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,11 @@
 $(document).ready(() => {
   $(".sidebar-wrapper").load("/components/sidebar.html");
   $(".navbar-wrapper").load("/components/navbar.html");
+  $(".footer-wrapper").load("/components/footer.html");
+  $(".arrow-up-wrapper").load("/components/arrow-up.html");
+});
+
+$(window).scroll(() => {
+  const hasScrolled = document.body.scrollTop > 20 || document.documentElement.scrollTop > 20;
+  $("#arrow-up").css("display", hasScrolled? "block" : "none")
 });
